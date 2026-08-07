f908ce0c51ad3b689ab00227ffd30cd9197e2af1
diegomazuru013 <diegomazuru@gmail.com>
2026-08-07 15:48:12 +0200

    Enhance bot with email generation and game suggestions
    Added functionality to generate email addresses and respond with game recommendations.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
-from gen_password import gen_psw
+
+from gen_password import gen_psw, gen_email
 
 
 # la variabile intents contiene i permessi al bot
@@ -23,10 +24,15 @@ async def on_message(message):
         await message.channel.send("\U0001f642")
     elif message.content.startswith("sono triste"):
         await message.channel.send("\U0001F622")
+    elif message.content.startswith("migliori giochi da giocare"):
+            await message.channel.send("i migliori giochi da giocare sono: Forza Horizon 6, il survival horror Resident Evil Requiem, l'avventura retrò Mina the Hollower, e la novità creativa Pokémon Pokopia.")
     elif message.content.startswith("genera password"):
         password = gen_psw()
         await message.channel.send(password)
+    elif message.content.startswith("genera email"):
+        email = gen_email()
+        await message.channel.send(email+"@gmail.com")
     else:
         await message.channel.send(message.content)
 
-client.run("token")
+client.run("token")  # inserisci il token del tuo bot
